1bf3ab0b8fdf8f429480b4292b109c49b3511c54
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2024-11-20 17:22:41 +0200

    Completed Quiz's CRUD operations. Added beta exception handler for quiz.

--- BackEnd/src/routes/quiz_route.py
+++ BackEnd/src/routes/quiz_route.py
@@ -1,6 +1,8 @@
+import logging
+from http import HTTPStatus
 from flask import Blueprint, request, jsonify
 from src.entities.Quiz import Quiz
-from src.services.quiz_service import create_quiz
+from src.services.quiz_service import *
 from src.entities.Question import Question
 
 #
@@ -13,37 +15,107 @@ QUIZ_URL = '/api/quizzes'
 quiz_blueprint = Blueprint('quiz',__name__, url_prefix= QUIZ_URL)
 
 #
-# Route to add a quiz into the Firestore Database
+#	Route to display list of quizzes
+#
+@quiz_blueprint.route('/', methods = ['GET'])
+def get_quizzes() -> jsonify:
+	logging.info("\tRoutes Layer ==>	Requesting quizzes list loading...")
+
+	# Retrieve quizzes
+	quizzes_data = get_quizzes_data()
+
+	# Verify if data exists
+	if quizzes_data is None:
+		return jsonify({"status:": "error",
+						"message": "No quizzes available"}), HTTPStatus.NOT_FOUND
+	
+	return jsonify({"quizzes": quizzes_data}), HTTPStatus.OK
+
+
+#
+#	Route to add a quiz into the Firestore Database
 #
 @quiz_blueprint.route('/',methods = ['POST'])
-def add_quiz():
+def add_quiz() -> jsonify:
+	logging.info("\tRoutes Layer ==>	Creating new quiz loading...")
 	try:
 		data = request.get_json()
-		if not data:
-			return jsonify({"error":"No data provided"}), 400
-		print("Incoming data",data)
 
 		# Extract and validate the fields
 		title = data.get('title')
 		description = data.get('description')
 		teacher = data.get('teacher')
-		questions = data.get('questions',[])
-		
-		if not title or description or teacher:
-			return jsonify({"error":"Missing requierd fields title,description or teacher"})
+		questions = data.get('questions',[])	
+
+		if title is None or description is None or teacher is None:
+			return jsonify({"status": "error",
+							"message": "Missing required fields title, description or teacher"}), HTTPStatus.BAD_REQUEST
 
 		if not isinstance(questions,list):
-			return jsonify({"error":"Questions must be a list"}), 400
+			return jsonify({"status": "error",
+				   			"message":"Quiz needs a list of question."}), HTTPStatus.BAD_REQUEST
 		
-		# Convert questions into Question objects 
-		questions = [Question.from_dict(q) if isinstance(q, dict) else q for q in questions]
-		quiz = Quiz(title = title,description = description,teacher = teacher,questions = questions)
-		result = create_quiz(quiz)
-		print("Result from create_quiz():",result)
+		# Convert questions into Question objects
+		questions = [Question.from_dict(question) if isinstance(question, dict) else question for question in questions]
+		quiz = Quiz(title, description, teacher, questions)
+		
+		# Call service function
+		create_quiz(quiz)
 
-		if "error" in result:
-			return jsonify(result), 400
-
-		return jsonify(result), 200
+		return jsonify({"status": "success", 
+						"message": "Quiz created successfully."}), HTTPStatus.OK
+	
 	except Exception as e:
-		return {"error": str(e)}
+		# Catch unexpected errors
+		return jsonify({"status": "error", "message": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
+	
+
+#
+#	Update quiz route
+#
+@quiz_blueprint.route("/<quiz_id>", methods = ['PUT'])
+def update_quiz(quiz_id: str) -> jsonify:
+	logging.info(f"\tRoutes Layer ==>	Updating quiz with id: {quiz_id} loading...")
+
+	try:
+		# Fetch updated data 
+		updated_data = request.get_json()
+		logging.debug(f"Display JSON data: {updated_data}")
+		
+		# Call service function
+		update_quiz_data(updated_data, quiz_id)
+
+		return jsonify({"status": "success", 
+						"message": "Quiz updated successfully."}), HTTPStatus.OK
+	
+	#
+	#	Except Handlings
+	#
+	except LookupError as e:
+		logging.error(f"\tRoutes Layer ==> Error: {str(e)}")
+		return jsonify({
+			"status": "error",
+			"message": str(e)
+		}), HTTPStatus.NOT_FOUND
+	
+	except Exception as e:
+			# Catch unexpected errors
+			return jsonify({"status": "error", "message": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
+	
+	
+#
+#	Delete quiz route
+#
+@quiz_blueprint.route("/<quiz_id>", methods = ['DELETE'])
+def delete_quiz(quiz_id: str) -> jsonify:
+		logging.info("\tRoutes Layer ==>	Deleting quiz loading...")
+		try:
+			# Call service function
+			delete_quiz_by_id(quiz_id)
+
+			return jsonify({"status": "success", 
+						"message": "Quiz deleted successfully."}), HTTPStatus.OK
+		
+		except Exception as e:
+			# Catch unexpected errors
+			return jsonify({"status": "error", "message": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
--- BackEnd/src/services/quiz_service.py
+++ BackEnd/src/services/quiz_service.py
@@ -1,15 +1,152 @@
+import logging
+from src.entities.Question import Question
 from src.database import db
 from src.entities.Quiz import Quiz
 
-def create_quiz(quiz: Quiz):
-    # Convert quiz to a dictionary
-    quiz_data = quiz.to_dict()
-    print("Quiz data:",quiz_data)
+COLLECTION_NAME = 'quizzes'
 
-    try:
-        # Add quiz to the Firestore Database
-        _,quiz_ref = db.collection("quizzes").add(quiz_data)
-        return {"message":f"Quizz with title {quiz.title} added with ID: {quiz_ref.id}"}
-    except Exception as e:
-        return {"error": str(e)}
+#
+#   Retrieve all quizzes
+#
+def get_quizzes_data() -> list[Quiz]:
+	logging.info("\tService Layer ==>	Retrieve quizzes loading...")
 
+	# Fetch data from databse
+	quizzes_data = db.collection(COLLECTION_NAME).stream()
+
+	# Create list of quizzes
+	quizzes_list = []
+	for quiz in quizzes_data:
+		quiz_dict =  quiz.to_dict()
+		quiz_dict['id'] = quiz.id
+		quizzes_list.append(quiz_dict)
+	
+	# Check if data exists
+	if not quizzes_list:
+		logging.warning("\tService Layer ==>	No quizzes in database.")
+		return None
+		
+	logging.info("\tService Layer ==>	Successfully retrieved quizzes list.")
+	return quizzes_list
+
+#
+# 	Retrieve quiz by id
+# 	
+def get_quiz_by_id(quiz_id: str) -> dict:
+	logging.info("\tService Layer ==>	Retrieve quiz by id loading...")
+
+	# Fetch data based on id
+	quiz_snapshot = db.collection(COLLECTION_NAME).document(quiz_id).get()
+	logging.debug(f"Retrieved quiz_data: {quiz_snapshot}")
+
+	# Check if data exists
+	if not quiz_snapshot.exists:
+		logging.warning(f"\tService Layer ==>	No data found for quiz with id: {quiz_id}")
+		return None
+
+	logging.info(f"\tService Layer ==>	Successfully retrieved quiz with id: {quiz_id}")
+	return quiz_snapshot.to_dict()
+
+####################################
+#
+#   CRUD Operations
+#
+####################################
+
+#
+#   Add
+#
+def create_quiz(quiz: Quiz) -> None:
+	logging.info("\tRoutes Layer ==>	Creating new quiz loading...")
+	# Convert quiz to a dictionary
+	quiz_data = quiz.to_dict()
+
+	try:
+		# Add quiz to the Firestore Database
+		db.collection("quizzes").add(quiz_data)
+		logging.info("\tService Layer ==>	Successfully created quiz.")
+	
+	except KeyError as e:
+		return {"error": f"Key missing: {str(e)}"}
+
+#
+#	Update
+#
+def update_quiz_data(updated_data: dict, quiz_id: str) -> None | LookupError:
+	logging.info("\tService Layer ==>	Updating quiz loading...")
+
+	try:		
+		# Retrieve data of quiz that gets updated
+		quiz_data = get_quiz_by_id(quiz_id)
+		
+		# Verify if data is None, return error log
+		if quiz_data is None:
+			logging.warning("\tService Layer ==> Quiz not found.")
+			raise LookupError(f"Quiz with ID '{quiz_id}' not found.")
+
+		# Update fields
+		for key in ['title', 'description', 'teacher']:
+			# Verify which field gets update
+			if key in updated_data:
+				quiz_data[key] = updated_data[key]
+		
+		'''
+			First, check if there are any new questions in the update data. 
+		For each question, ensure that all its properties—such as 
+		text, options, and correct_answers—are updated accordingly. 
+			Additionally, if there are new questions in the update data 
+		that do not already exist in the quiz, make sure to add them 
+		to the quiz object, keeping the quiz data properly updated and 
+		consistent.
+		'''
+
+		if 'questions' in updated_data:
+			logging.debug("New questions data found inside JSON.")
+			for updated_question in updated_data['questions']:
+
+				# Variable that help us to track update question
+				found = False
+				for question in quiz_data['questions']:
+
+					# Update question text
+					if question['text'] == updated_question['text']:
+
+						# Update details of question
+						question.update(updated_question)
+
+						# Mark that question was found
+						found = True
+						break
+
+				if not found:
+					# Case where question wasn't found, so we add it
+					quiz_data['questions'].append(updated_question)
+
+		# Retrieve reference of quiz that needs to be updated
+		quiz_ref = db.collection(COLLECTION_NAME).document(quiz_id)
+
+		# Update quiz in Firestore
+		quiz_ref.set(quiz_data)
+		logging.info("\tService Layer ==>	Successfully updated quiz.")
+
+	except KeyError as e:
+		return {"error": f"Key missing: {str(e)}"}
+
+
+#
+#	Delete
+#
+def delete_quiz_by_id(quiz_id: str) -> None:
+	try:
+		# Verify if quiz exists using id
+		get_quiz_by_id(quiz_id)
+
+		# Retrieve document reference
+		quiz_ref = db.collection(COLLECTION_NAME).document(quiz_id)
+
+		# Delete ref
+		logging.info("\tService Layer ==>	Quiz deleted successfully.")
+		quiz_ref.delete()
+
+	except KeyError as e:
+		return {"error": f"Key missing: {str(e)}"}
