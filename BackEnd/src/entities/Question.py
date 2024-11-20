f0f43a57fd07b4990e7decfb81016259c57d5ce9
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2024-11-20 17:21:51 +0200

    Added less complicated models init on empty lists

--- BackEnd/src/entities/Question.py
+++ BackEnd/src/entities/Question.py
@@ -1,29 +1,20 @@
-from src.entities.Option import Option
-
 class Question:
-    def __init__(self,text, options = None, correct_answers = None):
-        self.text = text
-        if options is None:
-            self.options = []
-        else:
-            self.options = list(options)
-        if correct_answers is None:
-            self.correct_answers = []
-        else:
-            self.correct_answers = list(correct_answers)
+	def __init__(self,text, options = None, correct_answers = None):
+		self.text = text
+		self.options = options or []
+		self.correct_answers = correct_answers or []
 
-    def to_dict(self):
-        return {
-            "text":self.text,
-            "options":[option.to_dict() for option in self.options],
-            "correct_answers":self.correct_answers
-        }
-    
-    @staticmethod
-    def from_dict(data):
-        options = [Option.from_dict(opt) for opt in data.get("options",[])]
-        return Question(
-            text = data["text"],
-            options = options,
-            correct_answers = data.get("correct_answers",[])
-        )
+	def to_dict(self):
+		return {
+			"text": self.text,
+			"options": self.options,
+			"correct_answers": self.correct_answers
+		}
+	
+	@staticmethod
+	def from_dict(data):
+		return Question(
+			text = data["text"],
+			options = data.get("options", []),
+			correct_answers = data.get("correct_answers",[])
+		)
