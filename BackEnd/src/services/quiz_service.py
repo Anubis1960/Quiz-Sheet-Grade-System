9677fc1a605fd474ebaccc71c02c4caf74d88fd9
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2024-11-21 12:23:42 +0200

    Added maximum length condition for quiz questions and answers

--- BackEnd/src/services/quiz_service.py
+++ BackEnd/src/services/quiz_service.py
@@ -2,8 +2,12 @@ import logging
 from src.entities.Question import Question
 from src.database import db
 from src.entities.Quiz import Quiz
+from src.exceptions.NoDataFoundError import NoDataFoundError
+from src.exceptions.StringLengthError import StringLengthError
 
 COLLECTION_NAME = 'quizzes'
+MAX_QUESTION_LENGTH = 250
+MAX_ANSWER_LENGTH = 100
 
 #
 #   Retrieve all quizzes
@@ -24,7 +28,7 @@ def get_quizzes_data() -> list[Quiz]:
 	# Check if data exists
 	if not quizzes_list:
 		logging.warning("\tService Layer ==>	No quizzes in database.")
-		return None
+		raise NoDataFoundError("No quizzes into database.")
 		
 	logging.info("\tService Layer ==>	Successfully retrieved quizzes list.")
 	return quizzes_list
@@ -61,13 +65,28 @@ def create_quiz(quiz: Quiz) -> None:
 	# Convert quiz to a dictionary
 	quiz_data = quiz.to_dict()
 
-	try:
-		# Add quiz to the Firestore Database
-		db.collection("quizzes").add(quiz_data)
-		logging.info("\tService Layer ==>	Successfully created quiz.")
+	#
+	#	Validations
+	#
 	
-	except KeyError as e:
-		return {"error": f"Key missing: {str(e)}"}
+	# Validate quiz questions length
+	for idx, question in enumerate(quiz_data['questions'], start = 1):
+
+		if len(question['text']) >=  MAX_QUESTION_LENGTH:
+			logging.warning(f"Question: {idx} exceeds the maximum length of {MAX_QUESTION_LENGTH} characters.")
+			raise StringLengthError(f"Question {idx} exceeds the maximum length of {MAX_QUESTION_LENGTH} characters.")
+		
+		# Validate question answers length
+		for idx, option in enumerate(question['options'], start = 1):
+
+			if len(option) >= MAX_QUESTION_LENGTH:
+				logging.warning(f"Option: {idx} exceeds the maximum length of {MAX_QUESTION_LENGTH} characters.")
+				raise StringLengthError(f"Option {idx} exceeds the maximum length of {MAX_QUESTION_LENGTH} characters.")
+		
+	# Add quiz to the Firestore Database
+	db.collection("quizzes").add(quiz_data)
+	logging.info("\tService Layer ==>	Successfully created quiz.")
+
 
 #
 #	Update
@@ -75,78 +94,70 @@ def create_quiz(quiz: Quiz) -> None:
 def update_quiz_data(updated_data: dict, quiz_id: str) -> None | LookupError:
 	logging.info("\tService Layer ==>	Updating quiz loading...")
 
-	try:		
-		# Retrieve data of quiz that gets updated
-		quiz_data = get_quiz_by_id(quiz_id)
-		
-		# Verify if data is None, return error log
-		if quiz_data is None:
-			logging.warning("\tService Layer ==> Quiz not found.")
-			raise LookupError(f"Quiz with ID '{quiz_id}' not found.")
+	# Retrieve data of quiz that gets updated
+	quiz_data = get_quiz_by_id(quiz_id)
+	
+	# Verify if data is None, return error log
+	if quiz_data is None:
+		logging.warning("\tService Layer ==> Quiz not found.")
+		raise LookupError(f"Quiz with ID '{quiz_id}' not found.")
 
-		# Update fields
-		for key in ['title', 'description', 'teacher']:
-			# Verify which field gets update
-			if key in updated_data:
-				quiz_data[key] = updated_data[key]
-		
-		'''
-			First, check if there are any new questions in the update data. 
-		For each question, ensure that all its properties—such as 
-		text, options, and correct_answers—are updated accordingly. 
-			Additionally, if there are new questions in the update data 
-		that do not already exist in the quiz, make sure to add them 
-		to the quiz object, keeping the quiz data properly updated and 
-		consistent.
-		'''
+	# Update fields
+	for key in ['title', 'description', 'teacher']:
+		# Verify which field gets update
+		if key in updated_data:
+			quiz_data[key] = updated_data[key]
+	
+	'''
+		First, check if there are any new questions in the update data. 
+	For each question, ensure that all its properties—such as 
+	text, options, and correct_answers—are updated accordingly. 
+		Additionally, if there are new questions in the update data 
+	that do not already exist in the quiz, make sure to add them 
+	to the quiz object, keeping the quiz data properly updated and 
+	consistent.
+	'''
 
-		if 'questions' in updated_data:
-			logging.debug("New questions data found inside JSON.")
-			for updated_question in updated_data['questions']:
+	if 'questions' in updated_data:
+		logging.debug("New questions data found inside JSON.")
+		for updated_question in updated_data['questions']:
 
-				# Variable that help us to track update question
-				found = False
-				for question in quiz_data['questions']:
+			# Variable that help us to track update question
+			found = False
+			for question in quiz_data['questions']:
 
-					# Update question text
-					if question['text'] == updated_question['text']:
+				# Update question text
+				if question['text'] == updated_question['text']:
 
-						# Update details of question
-						question.update(updated_question)
+					# Update details of question
+					question.update(updated_question)
 
-						# Mark that question was found
-						found = True
-						break
+					# Mark that question was found
+					found = True
+					break
 
-				if not found:
-					# Case where question wasn't found, so we add it
-					quiz_data['questions'].append(updated_question)
+			if not found:
+				# Case where question wasn't found, so we add it
+				quiz_data['questions'].append(updated_question)
 
-		# Retrieve reference of quiz that needs to be updated
-		quiz_ref = db.collection(COLLECTION_NAME).document(quiz_id)
+	# Retrieve reference of quiz that needs to be updated
+	quiz_ref = db.collection(COLLECTION_NAME).document(quiz_id)
 
-		# Update quiz in Firestore
-		quiz_ref.set(quiz_data)
-		logging.info("\tService Layer ==>	Successfully updated quiz.")
-
-	except KeyError as e:
-		return {"error": f"Key missing: {str(e)}"}
+	# Update quiz in Firestore
+	quiz_ref.set(quiz_data)
+	logging.info("\tService Layer ==>	Successfully updated quiz.")
 
 
 #
 #	Delete
 #
 def delete_quiz_by_id(quiz_id: str) -> None:
-	try:
-		# Verify if quiz exists using id
-		get_quiz_by_id(quiz_id)
 
-		# Retrieve document reference
-		quiz_ref = db.collection(COLLECTION_NAME).document(quiz_id)
+	# Verify if quiz exists using id
+	get_quiz_by_id(quiz_id)
 
-		# Delete ref
-		logging.info("\tService Layer ==>	Quiz deleted successfully.")
-		quiz_ref.delete()
+	# Retrieve document reference and delete it
+	db.collection(COLLECTION_NAME).document(quiz_id).delete()
+	logging.info("\tService Layer ==>	Quiz deleted successfully.")
+	
 
-	except KeyError as e:
-		return {"error": f"Key missing: {str(e)}"}
