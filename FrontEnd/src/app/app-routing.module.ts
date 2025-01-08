61e92dbd93fd6056610a572ee8f03815e950047b
Robert <robert.ilea03@e-uvt.ro>
2025-01-08 16:52:21 +0200

    Modules and styling

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { CreatePaperworkComponent } from './components/create-paperwork/create-paperwork.component';
+import { UploadPaperworkComponent } from './components/upload-paperwork/upload-paperwork.component';
 
 const routes: Routes = [
-  {path: '', component : HomePageComponent}
+  {path: '', component : HomePageComponent},
+  {path: 'create-paperwork', component : CreatePaperworkComponent},
+  {path: 'upload-paperwork', component : UploadPaperworkComponent}
 ];
 
 @NgModule({
