a7d58bb55d8d20fb97190be5ff784983b858a0cd
Anubis1960 <alexander.rk19@gmail.com>
2025-01-11 19:49:50 +0200

    fix form

--- FrontEnd/src/app/components/create-paperwork/create-paperwork.component.ts
+++ FrontEnd/src/app/components/create-paperwork/create-paperwork.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Quiz } from '../../models/quiz-model';
 import { Question } from '../../models/question-model';
 import { QuizService } from '../../services/quiz.service';
@@ -11,8 +11,8 @@ import { MessageService } from 'primeng/api';
   templateUrl: './create-paperwork.component.html',
   styleUrls: ['./create-paperwork.component.css']
 })
-export class CreatePaperworkComponent {
-  quizForm: FormGroup;
+export class CreatePaperworkComponent implements OnInit {
+  quizForm!: FormGroup;
   errorMessage: string = '';
   new_question_text: string = '';
 
@@ -22,15 +22,25 @@ export class CreatePaperworkComponent {
     private router: Router,
     private messageService: MessageService
   ) {
+  }
+
+  ngOnInit() {
     this.quizForm = this.fb.group({
       title: [null, Validators.required],
       description: [null, Validators.required],
-      teacher: [null, Validators.required],
-      questions: this.fb.array([])
+      questions: this.fb.array([
+        this.fb.group({
+          text: ['', Validators.required],
+          answers: this.fb.array([
+            this.fb.group({
+              a_text: ['', Validators.required],
+              is_correct: [false]
+            })
+          ]),
+        })
+      ])
     });
   }
-
-  ngOnInit() {}
 
   get questions() {
     return (this.quizForm.get('questions') as FormArray);
@@ -43,20 +53,28 @@ export class CreatePaperworkComponent {
   showMessage(severity: string, summary: string, detail: string): void {
     this.messageService.add({ severity, summary, detail });
   }
+
   addAnswer(questionIndex: number) {
-    const answerGroup = this.fb.group({
-      answer: ['', Validators.required],
-      isCorrect: [false]
-    });
-    this.getAnswers(questionIndex).push(answerGroup);
+    console.log('Adding answer to question:', questionIndex);
+    console.log(this.quizForm.value);
+    const answers = this.questions.at(questionIndex).get('answers') as FormArray;
+    answers.push(this.fb.group({
+      a_text: [null, Validators.required],
+      is_correct: [false]
+    }));
   }
-  addQuestion(questionText: string) {
-    const questionGroup = this.fb.group({
-      text: [questionText, Validators.required],
-      answers: this.fb.array([]),
-      correct_answers: this.fb.array([])
-    });
-    this.questions.push(questionGroup);
+
+  addQuestion() {
+    console.log(this.quizForm.value);
+    this.questions.push(this.fb.group({
+      text: [null, Validators.required],
+      answers: this.fb.array([
+        this.fb.group({
+          a_text: [null, Validators.required],
+          is_correct: [false]
+        })
+      ]),
+    }));
   }
 
   removeQuestion(questionIndex: number) {
@@ -68,26 +86,45 @@ export class CreatePaperworkComponent {
   }
 
 
-  toggleCorrectAnswer(questionIndex: number, answerIndex: number, event: any) {
-    const checked = event.target.checked;
-    const correctAnswersArray = this.questions.at(questionIndex).get('correct_answers') as FormArray;
-    if (checked) {
-      correctAnswersArray.push(this.fb.control(answerIndex));
-    } else {
-      const index = correctAnswersArray.value.indexOf(answerIndex);
-      if (index !== -1) {
-        correctAnswersArray.removeAt(index);
+  toggleCorrectAnswer(questionIndex: number, answerIndex: number) {
+    let is_correct = this.getAnswers(questionIndex).at(answerIndex).get('is_correct')?.get('value');
+    if (is_correct) {
+      if (is_correct.value) {
+        is_correct.setValue(false);
+      }
+      else {
+        is_correct.setValue(true);
       }
     }
   }
 
   createQuiz() {
-    console.log('Inside create_quiz...');
+    let title = this.quizForm.value.title;
+    let description = this.quizForm.value.description;
+    let teacher = 'teacher';
+    let questions = this.quizForm.value.questions.map((question: any) => {
+      let text = question.text;
+      let answers: string[] = question.answers.map((answer: any) => answer.a_text); // Collect answer texts
+      let correct_answers: number[] = question.answers
+        .map((answer: any, index: number) => {
+          if (answer.is_correct) {
+            return index;
+          }
+          return null;
+        })
+        .filter((index: number | null) => index !== null) as number[];
+      return {
+        text,
+        answers,
+        correct_answers
+      };
+    });
+    console.log('Creating quiz:', title, description, teacher, questions);
     this.quiz_service
       .post_quiz(
         this.quizForm.value.title,
         this.quizForm.value.description,
-        this.quizForm.value.teacher,
+        teacher,
         this.quizForm.value.questions
       )
       .subscribe(
